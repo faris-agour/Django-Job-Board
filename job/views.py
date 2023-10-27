d29cfd6a9873180b57bf589b64a74b8cb2e1a1b4
fares_agour <faresabouagour@gmail.com>
2023-10-27 18:51:13 +0200

    make simple views

--- job/views.py
+++ job/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Job
 
-# Create your views here.
+
+def job_list(request):
+    job_list = Job.objects.all()
+    context = {'jobs': job_list}
+    return render(request, "job/job_list.html", context)
+
+
+def job_details(request, id):
+    job_details = Job.objects.get(id=id)
+    context ={'job': job_details}
+
+    return render(request, "job/job_details.html", context)
