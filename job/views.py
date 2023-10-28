90f71b9bb8597b7b51deed93d9a459999ad15350
fares_agour <faresabouagour@gmail.com>
2023-10-28 14:54:17 +0200

    edit some front-end

--- job/views.py
+++ job/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from .models import Job
 
 
-def job_list(request):
-    job_list = Job.objects.all()
-    context = {'jobs': job_list}
-    return render(request, "job/job_list.html", context)
+def jobs(request):
+    jobs = Job.objects.all()
+    context = {'jobs': jobs}
+    return render(request, "job/jobs.html", context)
 
 
 def job_details(request, id):
