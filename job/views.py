00021dcab63f271f47e7328a273bd2c793f08fe6
fares_agour <faresabouagour@gmail.com>
2023-10-28 18:13:23 +0200

    pagination

--- job/views.py
+++ job/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from .models import Job
-
+from django.core.paginator import Paginator
 
 def jobs(request):
     jobs = Job.objects.all()
-    context = {'jobs': jobs}
+    paginator = Paginator(jobs, 1)  # Show 25 contacts per page.
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    context = {'jobs': page_obj}
     return render(request, "job/jobs.html", context)
 
 
