54a21a6c41257559edceffbb925d4edafc9d8065
fares-agour <faresabouagour@gmail.com>
2024-12-03 02:02:36 +0200

    Feat: Filter jobs optimization

--- job/filters.py
+++ job/filters.py
@@ -2,14 +2,21 @@ import django_filters
 
 from .models import Job
 
-
+from django.db.models import Q
 class JobFilter(django_filters.FilterSet):
-    title = django_filters.CharFilter(lookup_expr='icontains')
-    description = django_filters.CharFilter(lookup_expr='icontains')
+    search = django_filters.CharFilter(method='filter_search', label='Search')
     # salary = django_filters.NumberFilter()
     salary__lt = django_filters.NumberFilter(field_name='salary', lookup_expr='lt')
     salary__gt = django_filters.NumberFilter(field_name='salary', lookup_expr='gt')
 
     class Meta:
         model = Job
-        fields = ['title', 'description', 'jop_type', 'category', 'salary__lt','salary__gt','location']
+        fields = ['search', 'jop_type', 'category', 'salary__lt','salary__gt','location']
+
+    def filter_search(self, queryset, name, value):
+        """
+        Custom filter that searches both 'title' and 'description' fields.
+        """
+        if value:
+            return queryset.filter(Q(title__icontains=value) | Q(description__icontains=value))
+        return queryset
